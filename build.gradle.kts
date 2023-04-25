d50b0fbdcc28b7d4c40c035a505e79f1ad26cf6e
Leon Wang <lswx0925@gmail.com>
2023-04-25 11:53:26 +0800

    revert: alter gradle dist to binary only

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("cn.hutool:hutool-all:5.8.16")
     implementation("org.flywaydb:flyway-mysql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
     runtimeOnly("org.flywaydb:flyway-core:9.16.3")
-    implementation("cn.hutool:hutool-all:5.8.16")
 }
 
 java {
